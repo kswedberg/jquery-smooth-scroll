50c6d54f68acffe55a1f6a585b78bb30d61c32ae
Karl Swedberg <karl@englishrules.com>
2010-03-09 00:12:51 -0500

    refactored the plugin.

--- jquery.smooth-scroll.js
+++ jquery.smooth-scroll.js
@@ -1,119 +1,84 @@
-;(function($) {
+(function($) {
 // Animated Scrolling for Same-Page Links
 // @see http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links
 
+var version = '1.1';
+
+var locationPath = filterPath(location.pathname);
+
 $.fn.extend({
 	smoothScroll: function(options) {
-  var opts = $.extend({}, $.fn.smoothScroll.defaults, options),
-      locationPath = filterPath(location.pathname),
-      scrollElem = scrollableElement('html', 'body');
-      
-  this.each(function() {
-    var link = this,
-        $link = $(this),
-        hostMatch = ((location.hostname === link.hostname) || !link.hostname),
-        pathMatch = (filterPath(link.pathname) || locationPath) === locationPath,
-        thisHash = link.hash && link.hash.replace('#',''),
-        scrollTargetExists = thisHash && !!$('#' + thisHash).length;
+    
+    this.each(function() {
+      var opts = $.extend({}, $.fn.smoothScroll.defaults, options);
+      $(this).bind('click', function(event) {
+        var link = this, $link = $(this),
+            hostMatch = ((location.hostname === link.hostname) || !link.hostname),
+            pathMatch = opts.scrollTarget || (filterPath(link.pathname) || locationPath) === locationPath,
+            thisHash = link.hash && '#' + link.hash.replace('#',''),
+            include = true;
+        
 
-    if (opts.scrollTarget) {
-      if (!/#\S$/.test(opts.scrollTarget)) { opts.scrollTarget += ':first'; }
-      $link.data('scrollTarget', opts.scrollTarget);
-      
-    } else if (hostMatch && pathMatch && scrollTargetExists) {
-      var include = true,
-
-          exclude = opts.exclude,
-          elCounter = 0,
-          el = exclude.length,
-
-          excludeWithin = opts.excludeWithin,
-          ewlCounter = 0,
-          ewl = excludeWithin.length;
-       
-      while (include && elCounter < el) {
-        if ($link.is(exclude[elCounter++])) {
+        if (!opts.scrollTarget && (!hostMatch || !pathMatch || thisHash.length == 1) ) {
           include = false;
+        } else {
+          var exclude = opts.exclude, elCounter = 0, el = exclude.length;
+          while (include && elCounter < el) {
+            if ($link.is(exclude[elCounter++])) {
+              include = false;
+            }
+          }
+
+          var excludeWithin = opts.excludeWithin, ewlCounter = 0, ewl = excludeWithin.length;
+          while (include && ewlCounter < ewl) {
+            if ($link.parents(excludeWithin[ewlCounter++] + ':first').length) {
+              include = false;
+            }
+          }          
         }
-      }
-      while (include && ewlCounter < ewl) {
-        if ($link.parents(excludeWithin[ewlCounter++] + ':first').length) {
-          include = false;
+
+        if (include) {
+          opts.scrollTarget = opts.scrollTarget || thisHash;
+          event.preventDefault();
+          $.smoothScroll(opts);        
         }
-      }
-
-      if (include) {
-        $link.data('scrollTarget', '#' + thisHash);
-      }
-    }
-  });
-
-   
-  this.die('click.smoothscroll').live('click.smoothscroll', function(event) {
-    event.preventDefault();
-    $(this).smoothScroller();
-  });
-  
-  return this;
-  
-  // private functions
-  
-  // don't pass window or document
-  function scrollableElement(els) {
-    for (var i = 0, argLength = arguments.length; i < argLength; i++) {
-      var el = arguments[i],
-          $scrollElement = $(el);
-      if ($scrollElement.scrollTop() > 0) {
-        return el;
-      } else {
-        $scrollElement.scrollTop(1);
-        var isScrollable = $scrollElement.scrollTop() > 0;
-        $scrollElement.scrollTop(0);
-        if (isScrollable) {
-          return el;
-        }
-      }
-    }
-    return [];
-  }
-  
-  function filterPath(string) {
-    return string
-      .replace(/^\//,'')
-      .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-      .replace(/\/$/,'');
-  }
-  
-  function debug($obj) {
-    if (window.console && window.console.log) {
-      window.console.log($obj);
-    }
-  }
-},
-
-smoothScroller: function(link) {
-  var scrollTargetId = $(link).data('scrollTarget');
-  if (scrollTargetId) {
-    event.preventDefault();
-     
-    var scrollTargetOffset = $(scrollTargetId).offset().top;
-
-    $(scrollElem).animate({
-      scrollTop: scrollTargetOffset + opts.offset
-    }, 
-    {
-      duration: opts.speed,
-      easing: opts.easing
-    }, function() {
-      opts.afterScroll.call(link);
+      });
     });
+  
+    return this;
+
   }
-}
+  
 });
 
+$.smoothScroll = function(options, px) {
+  var opts,
+      scrollTargetOffset,
+      scrollElem = scrollableElement('html', 'body');
+  
+  if ( typeof options === 'number') {
+    opts = $.fn.smoothScroll.defaults;
+    scrollTargetOffset = options;
+  } else {
+    opts = $.extend({}, $.fn.smoothScroll.defaults, options);
+    scrollTargetOffset = $(opts.scrollTarget).offset().top;
+  }
+
+  $(scrollElem).animate({
+    scrollTop: scrollTargetOffset + opts.offset
+  }, 
+  {
+    duration: opts.speed,
+    easing: opts.easing
+  }, function() {
+    opts.afterScroll.call(link);
+  });
+
+};
+
+$.smoothScroll.version = version;
 // default options
 $.fn.smoothScroll.defaults = {
-  autobind: true,
   exclude: [],
   excludeWithin:[],
   offset: 0,
@@ -122,5 +87,36 @@ $.fn.smoothScroll.defaults = {
   easing: 'swing',
   speed: 400
 };
+
+
+// private functions
+
+// don't pass window or document
+function scrollableElement(els) {
+  for (var i = 0, argLength = arguments.length; i < argLength; i++) {
+    var el = arguments[i],
+        $scrollElement = $(el);
+    if ($scrollElement.scrollTop() > 0) {
+      return el;
+    } else {
+      $scrollElement.scrollTop(1);
+      var isScrollable = $scrollElement.scrollTop() > 0;
+      $scrollElement.scrollTop(0);
+      if (isScrollable) {
+        return el;
+      }
+    }
+  }
+  return [];
+}
+
+
+function filterPath(string) {
+  return string
+    .replace(/^\//,'')
+    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+    .replace(/\/$/,'');
+}
+
 
 })(jQuery);
