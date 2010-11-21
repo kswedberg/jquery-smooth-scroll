5c08756dcdb92b11dac8fe17e30a4bac297f1685
Karl Swedberg <kswedberg@gmail.com>
2010-11-20 19:09:45 -0500

    .scrollable() method now keeps all scrollable elements that were part of the jQuery set. New .firstScrollable() method selects the first scrollable element in the set. Added extra offset for scrollable elements other than html/body. Fixes issue #2.

--- jquery.smooth-scroll.js
+++ jquery.smooth-scroll.js
@@ -1,18 +1,22 @@
-/*
- * jQuery Smooth Scroll plugin
- * Version 1.2  (July 6, 2010)
- * @requires jQuery v1.3+
+/*!
+ * jQuery Smooth Scroll Plugin v1.3
  *
+ * Date: Sat Nov 20 18:12:52 2010 -0500
+ * Requires: jQuery v1.3+
+ *
+ * Copyright 2010, Karl Swedberg
  * Dual licensed under the MIT and GPL licenses (just like jQuery):
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  *
- */
-
+ *
+ *
+ *
+*/
 
 (function($) {
 
-var version = '1.2';
+var version = '1.3';
 
 var locationPath = filterPath(location.pathname);
 
@@ -24,21 +28,24 @@ $.fn.extend({
       if (this == document || this == window) { return; }
       var el = $(this);
       if ( el.scrollTop() > 0 ) {
-        scrollable = [this];
-        return false;
+        scrollable.push(this);
+        return;
       }
 
       el.scrollTop(1);
       scrolled  = el.scrollTop() > 0;
       el.scrollTop(0);
       if ( scrolled ) {
-        scrollable = [this];
-        return false;
+        scrollable.push(this);
+        return;
       }
 
     });
 
     return this.pushStack(scrollable);
+  },
+  firstScrollable: function() {
+    return this.scrollable().eq(0);
   },
 
 	smoothScroll: function(options) {
@@ -48,11 +55,11 @@ $.fn.extend({
       var link = this, $link = $(this),
           hostMatch = ((location.hostname === link.hostname) || !link.hostname),
           pathMatch = opts.scrollTarget || (filterPath(link.pathname) || locationPath) === locationPath,
-          thisHash = link.hash && '#' + link.hash.replace('#',''),
+          thisHash = link.hash,
           include = true;
 
 
-      if (!opts.scrollTarget && (!hostMatch || !pathMatch || thisHash.length == 1) ) {
+      if (!opts.scrollTarget && (!hostMatch || !pathMatch || !thisHash) ) {
         include = false;
       } else {
         var exclude = opts.exclude, elCounter = 0, el = exclude.length;
@@ -85,7 +92,11 @@ $.fn.extend({
 });
 
 $.smoothScroll = function(options, px) {
-  var opts, scrollTargetOffset, offPos = 'offset';
+  var opts, $scroller, scrollTargetOffset,
+      scrollerOffset = 0,
+      offPos = 'offset',
+      dirs = {top: 'Top', 'left': 'Left'},
+      aniprops = {};
 
   if ( typeof options === 'number') {
     opts = $.fn.smoothScroll.defaults;
@@ -106,11 +117,14 @@ $.smoothScroll = function(options, px) {
   }
   opts = $.extend({link: null}, opts);
 
-  var $scroller = opts.scrollElement || $('html, body').scrollable(),
-      dirs = {top: 'Top', 'left': 'Left'},
-      aniprops = {};
+  if ( opts.scrollElement ) {
+    $scroller = opts.scrollElement;
+    scrollerOffset = $scroller.scrollTop();
+  } else {
+    $scroller = $('html, body').firstScrollable();
+  }
 
-  aniprops['scroll' + dirs[opts.direction]] = scrollTargetOffset + opts.offset;
+  aniprops['scroll' + dirs[opts.direction]] = scrollTargetOffset + scrollerOffset + opts.offset;
   $scroller.animate(aniprops,
   {
     duration: opts.speed,
@@ -133,7 +147,7 @@ $.fn.smoothScroll.defaults = {
   offset: 0,
   direction: 'top', // one of 'top' or 'left'
   scrollElement: null, // jQuery set of elements you wish to scroll.
-                      //if null (default), $('html, body').scrollable() is used.
+                      //if null (default), $('html, body').firstScrollable() is used.
   scrollTarget: null, // only use if you want to override default behavior
   afterScroll: null,   // function to be called after window is scrolled. "this" is the triggering element
   easing: 'swing',
@@ -148,6 +162,5 @@ function filterPath(string) {
     .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
     .replace(/\/$/,'');
 }
-
 
 })(jQuery);
