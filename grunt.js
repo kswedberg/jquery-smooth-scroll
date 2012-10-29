080d3aeea028c1649841d1563f39e0f245fdc5d8
Karl Swedberg <kswedberg@gmail.com>
2012-10-29 09:37:20 -0400

    Rename package.json to smooth-scroll.jquery.json
    This makes it work with upcoming jQuery plugins site.

--- grunt.js
+++ grunt.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: '<json:package.json>',
+    pkg: '<json:smooth-scroll.jquery.json>',
     meta: {
       banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
