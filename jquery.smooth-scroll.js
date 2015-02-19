17e5166b1120114c5eaf4dac046d15dec563bfc5
Callum Macrae <callum@macr.ae>
2015-02-19 11:41:51 +0000

    Added proper commonjs support

--- jquery.smooth-scroll.js
+++ jquery.smooth-scroll.js
@@ -9,6 +9,9 @@
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
     define(['jquery'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // CommonJS
+    factory(require('jquery'));
   } else {
     // Browser globals
     factory(jQuery);
