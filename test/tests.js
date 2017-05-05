ee541b63373cbcee909adb022c66f82485893623
Karl Swedberg <kswedberg@gmail.com>
2017-05-05 11:44:15 -0400

    Lint tests.js

--- test/tests.js
+++ test/tests.js
@@ -4,6 +4,7 @@ QUnit.module('filterPath');
 
 QUnit.test('Link paths not location path', function(assert) {
   var locationPath = $.smoothScroll.filterPath(location.pathname);
+
   $('a[href*=#]').each(function() {
     assert.notEqual(locationPath, this.pathname);
   });
@@ -13,12 +14,14 @@ QUnit.module('scrollable');
 
 QUnit.test('Returns first scrollable element (html,body)', function(assert) {
   var scrollable = $('html,body').firstScrollable();
+
   assert.equal(scrollable.length, 1, 'One scrollable element is returned');
   assert.equal(scrollable[0], document.body, 'Scrollable element is <body>');
 });
 
 QUnit.test('Returns scrollable element (div#scrollable)', function(assert) {
   var scrollable = $('#scrollable').scrollable();
+
   assert.equal(scrollable.length, 1, 'One scrollable element is returned');
   assert.equal(scrollable[0].id, 'scrollable', 'Scrollable element is <div id="scrollable">');
 });
@@ -33,6 +36,7 @@ QUnit.test('Scrolls the #scrollable element, or not.', function(assert) {
     speed: 200,
     afterScroll: function() {
       var scrollTop = $('#scrollable').scrollTop();
+
       assert.equal(scrollTop, 0, '#scrollable element does not scroll to non-existant element');
 
       // Trigger the next scroll, which should actually work
@@ -49,6 +53,7 @@ QUnit.test('Scrolls the #scrollable element, or not.', function(assert) {
       speed: 200,
       afterScroll: function() {
         var scrollTop = $('#scrollable').scrollTop();
+
         assert.equal(scrollTop, 100, '#scrollable element scrolls');
         done();
       }
@@ -65,6 +70,7 @@ QUnit.test('Scrolls the #scrollable element to absolute or relative scroll posit
       speed: 100,
       afterScroll: function() {
         var scrollTop = $('#scrollable').scrollTop();
+
         assert.equal(scrollTop, 10, '#scrollable element scrolls back to 10');
 
         // Trigger the next scroll, which should actually work
@@ -78,6 +84,7 @@ QUnit.test('Scrolls the #scrollable element to absolute or relative scroll posit
     speed: 100,
     afterScroll: function() {
       var scrollTop = $('#scrollable').scrollTop();
+
       assert.equal(scrollTop, 15, '#scrollable element scrolls to 15');
 
       // Trigger the next scroll, which should actually work
@@ -94,6 +101,7 @@ QUnit.test('Scrolls the #scrollable element to absolute or relative scroll posit
       speed: 100,
       afterScroll: function() {
         var scrollTop = $('#scrollable').scrollTop();
+
         assert.equal(scrollTop, 25, '#scrollable element scrolls "+=10" to 25');
         done();
         finalScroll();
